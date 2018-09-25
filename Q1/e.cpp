850894c1330cfd7203814b1aa4daaba79c613965
abkabd <abdullayev.aybek6728@gmail.com>
2018-09-25 18:19:38 +0600

    upd

--- Q1/e.cpp
+++ Q1/e.cpp
@@ -1,34 +1,53 @@
 #include <bits/stdc++.h>
 
+#define MAXN (int)1e6+100
 using namespace std;
+
 
 void solve()
 {
-	int n;
-	int step[10000];
-	queue<int> q;
+	int n, cnt, x;
+	queue<int> q1, q2;
+	bool used[MAXN];
 	cin >> n;
-
-	q.push(n);
-	for(int i = 0; i<=n; i++){
-		step[i] = 1000100;
+	for(int i=0; i<=n; i++){
+		used[i] = false;
 	}
-	step[n] = 0;
-	while(!q.empty() || q.front() != 0){
-		int x = q.front();
-		q.pop();
+
+	q1.push(n);
+	q2.push(0);
+	while(!q1.empty() && q1.front() != 0){
+		x = q1.front();
+		cnt = q2.front();
+		q1.pop();
+		q2.pop();
+
+		if(x == 0){
+			break;
+		}
+
 		for(int i=2; i<=sqrt(x); i++){
 			if(x % i == 0)
 			{
-				q.push(x/i);
-				step[x/i] = min(step[x] + 1, step[x/i]);
+				if(!used[x/i]){
+					q1.push(x/i);
+					q2.push(cnt+1);
+					used[x/i] = true;
+				}
 			}
 		}
-		step[x-1] = min(step[x-1], step[x] + 1);
+		if(!used[x-1]){
+			q1.push(x-1);
+			q2.push(cnt+1);
+			used[x-1] = true;
+		}
 	}
 
-	cout << step[1] + 1 << endl;
+	cout << q2.front() << endl;
+	
+	
 }
+
 int main()
 {
 	int t, cs = 0;
